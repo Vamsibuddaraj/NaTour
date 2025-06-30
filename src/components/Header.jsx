aee796a8ebfebb346625203ec0d47f73adbfcfba
Vamsibuddaraj <vamsibuddaraju11@gmail.com>
2025-06-30 22:42:33 +0530

    header component for natour with basic animations and transitions

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css"
+import React from 'react'
+const Header = () => {
+  return (
+    <header>
+        <div className="header-box">
+            <h1 className="header-content">
+                <span id="main">Outdoors</span><span id="sub">is where life happens</span>
+            </h1>
+            <div className="header-btn">
+              <a href="#" className="btn">Discover our tour</a>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
